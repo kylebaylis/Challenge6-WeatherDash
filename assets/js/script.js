fc061884e29d473d780535cf12a4233883fc92f0
Kyle <kylejmbaylis@gmail.com>
2022-04-03 12:57:57 -0400

    can call city to get coordinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,16 @@
-var city = document.querySelector("cityName");
+var getWeather = function(city) {
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=53f36a2515b02d34176389d7c4f5de84";
 
-var searchFun = document.getElementById("search");
+    // get request for url
+    fetch(apiUrl).then(function(response) {
+    console.log(response);
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+}
 
-// fetch function for api call to get Coordinates
-var apiCallFun = function() {
-
-    var apiCallTest = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=53f36a2515b02d34176389d7c4f5de84";
-
-    fetch(apiCallTest).then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-};
-
-searchFun.addEventListener("click", apiCallFun());
+getWeather("london");
 
 
 
