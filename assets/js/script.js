ac5cb632fb525ec3d3160902a5cca8d11b5fc232
Kyle <kylejmbaylis@gmail.com>
2022-04-03 12:53:02 -0400

    testing how to get api call to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@ var city = document.querySelector("cityName");
 
 var searchFun = document.getElementById("search");
 
-// fetch function for api call
+// fetch function for api call to get Coordinates
 var apiCallFun = function() {
 
     var apiCallTest = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=53f36a2515b02d34176389d7c4f5de84";
-    
+
     fetch(apiCallTest).then(response => {
         return response.json();
     })
